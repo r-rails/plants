1c45bcae55b128f45981255972535fc9e3d4b3af
Czepesch <czepesch@gmail.com>
2022-07-08 00:15:12 +0200

    add http basic auth for admin dashboard

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,11 +6,14 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    before_action :authenticate_admin
+    http_basic_authenticate_with name: Rails.application.credentials.dashboard.username,
+      password: Rails.application.credentials.dashboard.password
+    
+    # before_action :authenticate_admin
 
-    def authenticate_admin
-      # TODO Add authentication logic here.
-    end
+    # def authenticate_admin
+    #   # TODO Add authentication logic here.
+    # end
 
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.
