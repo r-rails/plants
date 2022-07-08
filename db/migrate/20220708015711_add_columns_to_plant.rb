c7159efc5bdb75d9d2fa55ca36297d7d99dec880
Pierre Penel <penel.pierre@gmail.com>
2022-07-07 22:00:38 -0400

    Add new fields for Plant model

---
+++ db/migrate/20220708015711_add_columns_to_plant.rb
@@ -0,0 +1,10 @@
+class AddColumnsToPlant < ActiveRecord::Migration[7.0]
+  def change
+    add_column :plants, :img, :string
+    add_column :plants, :climate, :string
+    add_column :plants, :ideallight, :string
+    add_column :plants, :watering, :string
+    add_column :plants, :category, :string
+    add_column :plants, :latin, :string
+  end
+end
