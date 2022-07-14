0e45f7aa2009ed2da90df232439cbc44610ca9a0
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2022-07-14 18:03:55 +0100

    ad dependent destroy on join association

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,5 +1,6 @@
 class Plant < ApplicationRecord
   has_one_attached :img
   has_many :garden_plants
-  has_many :users, through: :garden_plants
+  has_many :users, through: :garden_plants, dependent: :destroy
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :garden_plants
-  has_many :plants, through: :garden_plants
+  has_many :plants, through: :garden_plants, dependent: :destroy
 end
