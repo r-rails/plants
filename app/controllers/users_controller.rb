c20a4ae458d2f51629e9011fa5d20c6458ee0937
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2022-07-15 21:01:52 +0100

    create user page that will display plants in a User"s garden

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+
+class UsersController < ApplicationController
+  
+  def show
+    @user = User.includes(:plants).where(id: params[:id]).first
+    @plants = @user.plants.page(params[:page]).per(10)
+    if !@user
+      flash[:error] = "User not found"
+      redirect_to root_path
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, only: :show
   devise_for :users
   namespace :admin do
       resources :plants
