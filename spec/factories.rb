047b63175cc1066eeef9d562ad58034c7dd8f8f0
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2022-07-15 12:40:39 +0100

    created factory for plant model

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,18 @@
-
 FactoryBot.define do
   factory(:user) do
     email { 'abc@example.com' }
     password { 'password' }
     password_confirmation { 'password' }
     username { 'john_doe' } 
+  end
+  
+  factory(:plant) do
+    description { Faker::Lorem.sentence }
+    name { Faker::Lorem.word }
+    climate { "Sunnytime" }
+    latin { "Aeschynanthus lobianus" }
+    category { "Hanging" }
+    ideallight { "Bright light" }
+    watering { "Keep moist between watering. Can be a bit dry between watering" }
   end
 end
