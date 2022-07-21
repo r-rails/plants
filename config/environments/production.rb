e4b79fb4b7212017589a8d4793165a06a5cdf294
Czepesch <czepesch@gmail.com>
2022-07-21 12:07:05 +0200

    add confirmable to users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # mailer settings for Devise
+  config.action_mailer.perform_deliveries = true
   config.action_mailer_default_url_options = { host: 'http://plants.23.88.117.56.sslip.io/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
