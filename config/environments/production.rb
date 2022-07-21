567cac96337c1a3e27635ada1bf8fa38ccb3df9b
Czepesch <czepesch@gmail.com>
2022-07-21 10:52:11 +0200

    mailer settings test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # mailer settings for Devise
+  config.action_mailer_default_url_options = { host: 'http://plants.23.88.117.56.sslip.io/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    authentication: :plain,
+    enable_starttls_auto: true,
+    user_name: Rails.application.credentials.gmail.user,
+    password: Rails.application.credentials.gmail.password
+    }
 end
