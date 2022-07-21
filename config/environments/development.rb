53295ad20bc0387a8bb35baee6afaff0068f301c
Czepesch <czepesch@gmail.com>
2022-07-21 13:13:38 +0200

    remove smtp settings from development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,17 +68,4 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # mailer settings for Devise
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer_default_url_options = { host: 'http://plants.23.88.117.56.sslip.io/' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'gmail.com',
-    authentication: :plain,
-    enable_starttls_auto: true,
-    user_name: Rails.application.credentials.gmail.user,
-    password: Rails.application.credentials.gmail.password
-    }
 end
