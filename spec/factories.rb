9e72f471a768a1cce5f3308bd1069b1b3fdbd9ea
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2022-08-01 10:35:03 +0100

    fixed broken tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,36 +1,24 @@
 FactoryBot.define do
-  # factory :comment do
-  #   user { nil }
-  #   body { "MyText" }
-  #   commentable { nil }
-  #   deleted_at { "2022-07-30 18:15:24" }
-  # end
-
-  # factory :comment do
-  #   plant { nil }
-  #   user { nil }
-  # end
-
   factory(:user) do
     email { Faker::Internet.email }
-    password { "password" }
-    password_confirmation { "password" }
-    username { "john_doe" }
+    password { 'password' }
+    password_confirmation { 'password' }
+    username { Faker::Name.name }
   end
 
   factory(:plant) do
     description { Faker::Lorem.sentence }
     name { Faker::Lorem.word }
-    climate { "Sunnytime" }
-    latin { "Aeschynanthus lobianus" }
-    category { "Hanging" }
-    ideallight { "Bright light" }
-    watering { "Keep moist between watering. Can be a bit dry between watering" }
+    climate { 'Sunnytime' }
+    latin { 'Aeschynanthus lobianus' }
+    category { 'Hanging' }
+    ideallight { 'Bright light' }
+    watering { 'Keep moist between watering. Can be a bit dry between watering' }
   end
 
-  # factory(:comment) do
-  #   body { Faker::Lorem.sentence(word_count: 7) }
-  #   user
-  #   plant
-  # end
+  factory(:comment) do
+    body { Faker::Lorem.sentence(word_count: 7) }
+    commentable { nil}
+    deleted_at { nil }
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,44 @@
-require "rails_helper"
+RSpec.describe Comment, :type => :model do 
+  describe "comment creation by Comments model" do 
+    context "with validated attributes" do
+      let!(:user) { create(:user)}
+      let!(:commentable) { create(:plant) }
+      let!(:comment) { create(:comment, user_id: user.id, commentable_id: commentable.id, commentable_type: "Plant") }
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+      it "creates a valid comment object into the database" do
+        expect(comment.valid?).to be true
+        expect(Comment.count).to eq(1)
+      end
+    end
+
+    context "when plant_id is not present" do
+      let!(:comment) { build(:comment) }
+
+      it "does not create a new comment" do
+        expect(comment.valid?).to be false
+        expect(Comment.count).to eq(0)
+      end
+    end
+
+    context "when user_id is not present" do
+      let!(:comment) { build(:comment, user: nil) }
+
+      it "does not create a new comment" do
+        expect(comment.valid?).to be false
+        expect(Comment.count).to eq(0)
+      end
+    end
+
+    context "Model Associations" do
+      it "should belong to a commentable" do
+        commentable_association = Comment.reflect_on_association(:commentable)
+        expect(commentable_association.macro).to eq(:belongs_to)
+      end
+
+      it "should belong to a User model" do
+        user_association = Comment.reflect_on_association(:user)
+        expect(user_association.macro).to eq(:belongs_to)
+      end
+    end
+  end
 end
