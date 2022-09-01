37e1337e91cef3d502509bb521f633de87af7a15
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2022-09-01 16:44:07 +0100

    removed stimulus connect method

--- app/javascript/controllers/tabclick_controller.js
+++ app/javascript/controllers/tabclick_controller.js
@@ -1,10 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  connect() {
-    console.log("connected");
-  }
-
   swapclass(event) {
     var tabs = [...document.querySelectorAll(".house-plant-tabs")];
 
