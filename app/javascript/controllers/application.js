d862be3f2b245bf268e6e8e92aed3da911b52741
Pipen95 <penel.pierre@gmail.com>
2022-09-01 15:13:00 -0400

    Turbo Frame  fixed

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import "controllers"
-import 'swiper/swiper-bundle.min.css'
 
 const application = Application.start()
-
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
