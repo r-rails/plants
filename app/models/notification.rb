809f321c21eab35a38b5137e0f0a2a44a68635d0
Czepesch <czepesch@gmail.com>
2022-09-09 08:39:46 +0200

    counter update on notification create

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -6,7 +6,16 @@ class Notification < ApplicationRecord
 
   default_scope { where("deleted_at > ?", 20.seconds.ago).or(where(deleted_at: nil)) }
 
-  after_create_commit :broadcast_to_recipient
+  after_create_commit :update_counter, :broadcast_to_recipient
+
+  def update_counter
+    broadcast_update_to(
+      recipient,
+      :counter,
+      target: "counter",
+      html: "#{recipient.notifications.not_deleted.unread.count >= 9 ? "9+" : recipient.notifications.not_deleted.unread.count }"
+    )
+  end
 
   def broadcast_to_recipient
     broadcast_append_later_to(
