a6dffbf00963bd4a84ba2dc254fa4e192bfd1831
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2022-09-09 13:45:22 +0100

    model spec for notification creation

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,16 @@
-require "rails_helper"
-
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "notification creation by the model" do
+    context "when a comment is made on a plant with users" do
+      let!(:plant) { create(:plant) }
+      let!(:users) { create_list(:user, 6) }
+      let!(:garden_plants) { users.each { |user| create(:garden_plant, plant_id: plant.id, user: user) } }
+      it "created notifications for all plant users except the comment user" do
+        comment_1 = create(:comment, commentable_id: plant.id, user_id: users.first.id)
+        
+        expect(Notification.count).to eq(5)
+        expect(Notification.ids).not_to include(users.first.id)
+        
+      end
+    end
+  end
 end
