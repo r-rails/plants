323886d7e46eae704c55521bbf203952ffec5465
Czepesch <czepesch@gmail.com>
2022-09-19 11:19:44 +0200

    hide secrets

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  def after_sign_out_path_for(_resource_or_scope)
-    new_user_session_path
-  end
+  # def after_sign_out_path_for(_resource_or_scope)
+  #   new_user_session_path
+  # end
   
-  def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || root_path
-  end
+  # def after_sign_in_path_for(resource_or_scope)
+  #   stored_location_for(resource_or_scope) || root_path
+  # end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
