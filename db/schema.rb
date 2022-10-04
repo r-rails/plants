9d60e643674b1be156843cb201b668a9aa9bf276
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2022-10-04 14:47:57 +0100

    plants count initiated in users table to avoid multiple db hits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_04_133401) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_04_134047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -266,6 +266,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_04_133401) do
     t.string "uid"
     t.integer "status", default: 0
     t.integer "comments_count", default: 0
+    t.integer "plants_count", default: 0
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
